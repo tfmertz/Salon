ae7640fc9c7888f7e2ec14c540c82c57708b40da
Tom Mertz <tom.mertz@verizon.net>
2015-03-20 09:33:02 -0700

    Add basic test layout

---
+++ src/Stylist.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Stylist
+    {
+        private $id;
+        private $name;
+
+        function __construct($stylist, $id = null)
+        {
+            $this->name = $stylist;
+            $this->id = $id;
+        }
+
+        
+    }
+
+ ?>
---
+++ tests/StylistTest.php
@@ -0,0 +1,16 @@
+<?php
+
+    require_once __DIR__.'src/Stylist.php';
+
+
+
+    class StylistTest extends PHPUnit_Framework_TestCase
+    {
+
+        
+
+    }
+
+
+
+ ?>
